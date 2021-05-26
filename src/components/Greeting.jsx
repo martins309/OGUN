676ed341e6ea69cf1dd35506f4801b59fc82c9ee
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-25 21:29:21 -0400

    created the first component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+const greeting = () => {
+    return (
+        <div>
+            <h1>This is OGUN </h1>
+            <h4>The refactored version of git ript for me teehee</h4>
+        </div>
+    )
+}
+
+export default greeting;
